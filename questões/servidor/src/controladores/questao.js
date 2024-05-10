cf50ac6187ecccb3bc24c58a712098ffe05f1c6c
lucasroqe <alvim.lucas2@hotmail.com>
2024-05-09 22:26:25 -0300

    Adiciona comentários de tabelas

--- questões/servidor/src/controladores/questao.js
+++ questões/servidor/src/controladores/questao.js
@@ -11,3 +11,31 @@ async function listar(req, res) {
 
 // Exporta as funções
 module.exports = { listar };
+
+/*
+Comandos para crias as tabelas tbcertificado e tbcertificadoquestao:
+
+
+CREATE TABLE tbCertificado (
+  id_usuario INT NOT NULL,
+  data_avaliacao TIMESTAMP NULL,
+  score DECIMAL(5,2) NULL,
+  PRIMARY KEY(id_usuario),
+  CONSTRAINT fk_usuario
+      FOREIGN KEY(id_usuario) 
+        REFERENCES tbUsuario(id_usuario)
+);
+
+CREATE TABLE tbCertificadoQuestao (
+  id_usuario INT NOT NULL,
+  id_questao INT NOT NULL,
+  resposta_usuario BOOL NULL,
+  PRIMARY KEY(id_usuario, id_questao),
+  CONSTRAINT fk_certificado
+      FOREIGN KEY(id_usuario) 
+        REFERENCES tbCertificado(id_usuario),
+  CONSTRAINT fk_questao
+      FOREIGN KEY(id_questao) 
+        REFERENCES tbQuestao(id_questao)
+);
+*/
